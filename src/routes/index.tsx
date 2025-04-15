244a528c2976ce87f13b465046bb3fdec46ff3bd
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2025-04-15 09:06:08 -0300

    chore: shadcn setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -8,6 +9,7 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button>Click me</Button>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
@@ -10,4 +11,9 @@ export default defineConfig({
 		tailwindcss(),
 		react(),
 	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
