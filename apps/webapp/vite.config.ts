cda0b0a7ef7e56351ccb5c44ebee8e177d9e8d99
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2025-04-16 08:55:06 -0300

    chore: create monorepo with turbopack

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,8 +1,8 @@
 import path from "path";
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
