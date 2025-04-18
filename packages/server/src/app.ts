afcdb10ad3ef28209890095ef41b0c7c698d50eb
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2025-04-18 13:22:30 -0300

    feat: add tRPC panel to test endpoint

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware } from "@hono/clerk-auth";
 import { trpcServer } from "@hono/trpc-server";
 import { Hono } from "hono";
+import { env } from "hono/adapter";
 import { csrf } from "hono/csrf";
 import { logger } from "hono/logger";
 import { appRouter } from "./routers/_app";
@@ -11,6 +12,26 @@ const app = new Hono();
 app.use(csrf());
 app.use("*", logger());
 app.use("*", clerkMiddleware());
+
+app.use("/api/trpc/panel", async (ctx) => {
+	const { ENVIRONMENT } = env<{ ENVIRONMENT: string }>(ctx);
+
+	if (ENVIRONMENT !== "development") {
+		return ctx.text("Not Found", 404);
+	}
+
+	const { renderTrpcPanel } = await import("trpc-ui");
+
+	return ctx.html(
+		renderTrpcPanel(appRouter, {
+			url: "http://localhost:8787/api/trpc",
+			meta: {
+				title: "tRPC Panel",
+				description: "Playground to test tRPC endpoints.",
+			},
+		}),
+	);
+});
 
 app.use(
 	"/api/trpc/*",
